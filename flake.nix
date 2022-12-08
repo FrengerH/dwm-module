3293a527b2412741c80948799c3bb6510f44f552
FrengerH <frengerh@hfmail.nl>
2022-12-08 12:28:56 +0100

    dwm-module

--- flake.nix
+++ flake.nix
@@ -1,7 +1,21 @@
 {
   description = "DWM for Nix";
 
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
   outputs = { self, nixpkgs, ... }: {
+    # system = "x86_64-linux";
+
+    # pkgs = import nixpkgs {
+    #   inherit system;
+    #   config = { 
+    #     allowUnfree = true; 
+    #     pulseaudio = true;
+    #   };
+    # };   
+
     dwm-module = import ./dwm-module;
   };
 }
