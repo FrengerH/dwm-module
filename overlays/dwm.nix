b4912b15a0b901b001758561db6ab692f089f58b
FrengerH <frengerh@hfmail.nl>
2022-12-08 13:11:08 +0100

    dwm-module

--- overlays/dwm.nix
+++ overlays/dwm.nix
@@ -7,9 +7,9 @@ in
 {
   dwm = super.dwm.overrideAttrs(oldAttrs: rec {
     src = super.fetchFromGitHub {
-      owner = "FrengerH"
-      repo = "dwm"
-      rev = "d39e2f3441fe18aba8d1a62c08918a411ec6f237"
+      owner = "FrengerH";
+      repo = "dwm";
+      rev = "d39e2f3441fe18aba8d1a62c08918a411ec6f237";
     };
 
     patches = [
