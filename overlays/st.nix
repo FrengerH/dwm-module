38f6537d8126bb239265da00eee3bc0c119d3a5b
FrengerH <frengerh@hfmail.nl>
2022-12-08 13:25:57 +0100

    dwm-module

--- overlays/st.nix
+++ overlays/st.nix
@@ -7,6 +7,13 @@ let
 in
 {
   st = super.st.overrideAttrs(olsattrs: rec {
+    src = super.fetchFromGitHub {
+      owner = "FrengerH";
+      repo = "st";
+      rev = "7fb0c0cc681f36be2ad12091ef93a41671f32738";
+      sha256 = "";
+    };
+
     patches = [
       ./patches/st.diff
     ];
