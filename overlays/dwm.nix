9ca20d135e3e2b83d8bb10f160b4c0d9a14ee91f
FrengerH <frengerh@hfmail.nl>
2022-12-08 13:15:39 +0100

    dwm-module

--- overlays/dwm.nix
+++ overlays/dwm.nix
@@ -10,6 +10,7 @@ in
       owner = "FrengerH";
       repo = "dwm";
       rev = "d39e2f3441fe18aba8d1a62c08918a411ec6f237";
+      sha256 = "sha256-pd1yi+DQ7xEV0iDyX+jC4KtcsfnqTH5ZOmPw++gSt8E=";
     };
 
     patches = [
