3fc24d7ab84a8535c585f8fb5ae844ce81f60b63
FrengerH <frengerh@hfmail.nl>
2022-12-08 21:22:41 +0100

    dwm-module

--- overlays/rofi.nix
+++ overlays/rofi.nix
@@ -6,7 +6,7 @@ let
   theme = builtins.toFile "rofi-theme" (builtins.readFile ../theme/rofi-theme.rasi);
 in
 {
-  rofi = super.st.overrideAttrs(oldAttrs: rec {
+  rofi = super.rofi.overrideAttrs(oldAttrs: rec {
     buildInputs = oldAttrs.buildInputs or [] ++ [ pkgs.makeWrapper ];
 
     postInstall = oldAttrs.postInstall or "" + ''
