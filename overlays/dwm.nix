5a8a186fdcf2dd5b94f1382682997fd2b30e601c
FrengerH <frengerh@hfmail.nl>
2022-12-08 12:54:34 +0100

    dwm-module

--- overlays/dwm.nix
+++ overlays/dwm.nix
@@ -6,7 +6,7 @@ let
 in
 {
   dwm = super.dwm.overrideAttrs(oldAttrs: rec {
-    src = super.fetchurl {
+    src = fetchurl {
       url = "https://dl.suckless.org/dwm/dwm-6.3.tar.gz";
       sha256 = "03hirnj8saxnsfqiszwl2ds7p0avg20izv9vdqyambks00p2x44p";
     };
