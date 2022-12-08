8bc16829f80618d4a032def312e5a8a70627690f
FrengerH <frengerh@hfmail.nl>
2022-12-08 13:30:55 +0100

    dwm-module

--- overlays/st.nix
+++ overlays/st.nix
@@ -11,7 +11,7 @@ in
       owner = "FrengerH";
       repo = "st";
       rev = "7fb0c0cc681f36be2ad12091ef93a41671f32738";
-      sha256 = "";
+      sha256 = "sha256-pULT784097GkY+rXElCCYLExJ5ORxYlJ5qeXCojUx0c=";
     };
 
     patches = [
