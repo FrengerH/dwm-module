51b06f2f3630088562e2bb1b3cd933bda4e598c3
FrengerH <frengerh@hfmail.nl>
2022-12-09 15:16:09 +0100

    dwm-module

--- overlays/tmux-dracula.nix
+++ overlays/tmux-dracula.nix
@@ -11,7 +11,7 @@ in
       owner = "FrengerH";
       repo = "tmux-dracula";
       rev = "a23870719534a33f14021cb46bfa160f1b6ab542";
-      sha256 = "";
+      sha256 = "sha256-MYV0A4VfzLL/HewaY8Oefqeer4V8iAyk81CfJZCjxTY=";
     };
   });
 }
